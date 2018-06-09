fd3ea4b3ab9b1196765a5ed262aaca3f3affe22e
Piotr-Augustynski <augustynskipiotr@gmail.com>
2018-06-09 09:21:32 +0200

    change security

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'wicek'
-  config.api_key = '962711316965987'
-  config.api_secret = 'kDqZ3EQdoUlRxZ-2ZS-8c3FerPw'
-  config.secure = true
-  config.cdn_subdomain = true
-end
